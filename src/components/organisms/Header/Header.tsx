cc4ce403b2d09951d8cffb32cf3f16a952f68b14
Max Tovstopiatyi <maxrori228@gmail.com>
2025-09-15 00:40:50 +0300

    feat(DEV-7): make a burger menu button

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="flex items-center justify-between h-[48px] pl-4 border-b shadow-[0px_1px_0px_0px_#E2E6E9] gap-8">
       <div className="flex gap-4 items-center">
@@ -9,28 +11,28 @@ export const Header: React.FC = () => {
           className="pt-[13px] pb-[13px]"
         >
           <img
-            className="w-[64px] h-[22px]"
+            className="w-[64px] h-[22px] block"
             src="img/header-logo.png"
             alt="Nice gadgets"
           />
         </a>
 
-        <nav className="hidden sm:flex justify-start gap-8 h-full flex items-center text-sm text-black no-underline">
+        <nav className="hidden sm:flex justify-start gap-8 h-full flex items-center no-underline">
           <a
             href="#"
-            className="h-full leading-[48px]"
+            className="h-full leading-[48px] font-mont uppercase font-extrabold"
           >
             Home
           </a>
           <a
             href="#"
-            className="h-full leading-[48px]"
+            className="h-full leading-[48px] font-mont uppercase font-extrabold"
           >
             Products
           </a>
           <a
             href="#"
-            className="h-full leading-[48px]"
+            className="h-full leading-[48px] font-mont uppercase font-extrabold"
           >
             Contact
           </a>
@@ -38,14 +40,22 @@ export const Header: React.FC = () => {
       </div>
 
       <a
+        onClick={() => setIsOpen(!isOpen)}
         href="#"
         className="sm:hidden shadow-[-1px_0px_0px_0px_#E2E6E9] p-4 inline-flex items-center gap-4 no-underline"
       >
-        <img
-          className="w-[16px] h-[16px]"
-          src="img/burger-menu.svg"
-          alt=""
-        />
+        {isOpen ?
+          <img
+            className="w-[16px] h-[16px]"
+            src="img/burger-menu-close.svg"
+            alt="close"
+          />
+        : <img
+            className="w-[16px] h-[16px]"
+            src="img/burger-menu.svg"
+            alt="menu"
+          />
+        }
       </a>
 
       <div className="hidden sm:flex">
