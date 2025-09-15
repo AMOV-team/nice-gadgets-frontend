3c82ec2e27a0f9ec10ed9e998c6f323bb1049182
Vladyslav Sulyma <sulymavladyslav@gmail.com>
2025-09-15 23:27:43 +0300

    point commit

--- src/components/pages/FavoritesPage.tsx
+++ src/components/pages/FavoritesPage.tsx
@@ -8,11 +8,12 @@ const testFavorites: Product[] = products.slice(0, 10);
 export const FavoritesPage: React.FC = () => {
   return (
     <div className="grid grid-cols-4 sm:grid-cols-12 [@media(min-width:988px)]:grid-cols-18 xl:grid-cols-24 gap-x-4 gap-y-[40px]">
-      <h1 className="text-h1 font-bold text-custom-primary col-span-4 sm:col-span-12 xl:col-span-24">
-        Favorites
-      </h1>
-
-      {/* <p className=''></p> */}
+      <div className="col-span-4 sm:col-span-12 xl:col-span-24">
+        <h1 className="text-h1 font-bold text-custom-primary">Favorites</h1>
+        <p className="font-semibold text-custom-secondary text-body">
+          {testFavorites.length} items
+        </p>
+      </div>
 
       {testFavorites.map((product) => (
         <div
