1bab39dd438077ca334400ef06d23d3d63c24d12
Max Tovstopiatyi <maxrori228@gmail.com>
2025-09-19 17:00:36 +0300

    feat(DEV-38): added favourites, cart and breadcrumb translation

--- src/components/pages/FavoritesPage.tsx
+++ src/components/pages/FavoritesPage.tsx
@@ -3,24 +3,29 @@ import { ProductCard } from '../molecules/ProductCard/ProductCard';
 import { GridContainer } from '../atoms/GridContainer';
 import { Breadcrumb } from '../molecules/Breadcrumb/Breadcrumb.tsx';
 import { useFavorites } from '../../hooks/useFavorites.ts';
+import { useTranslation } from 'react-i18next';
 
 export const FavoritesPage: React.FC = () => {
   const { favorites } = useFavorites();
+  const count = favorites.length;
+  const { t } = useTranslation();
 
   return (
     <GridContainer>
       <div className="col-span-4 sm:col-span-12 xl:col-span-24">
         <Breadcrumb />
-        <h1 className="text-h1 font-bold">Favorites</h1>
+        <h1 className="text-h1 font-bold">{t('favorites')}</h1>
         <p className="font-semibold text-custom-secondary text-body">
-          {favorites.length} items
+          {count === 0 ?
+            ``
+          : `${count} ${count === 1 ? `${t('item')}` : `${t('items')}`}`}
         </p>
       </div>
 
       {favorites.length === 0 ?
         <div className="col-span-4 sm:col-span-12 xl:col-span-24 text-center py-16">
           <p className="text-custom-secondary text-body">
-            No favorites yet. Start adding products to your favorites!
+            {t('empty-favourites')}
           </p>
         </div>
       : favorites.map((product) => (
