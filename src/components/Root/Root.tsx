b1a8308de2c4cbd15bd7f1195902711893f209ff
Vladyslav Sulyma <sulymavladyslav@gmail.com>
2025-09-19 10:38:17 +0300

    fixed conflicts from merge 69

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -18,106 +18,102 @@ import { CartProvider } from 'react-use-cart';
 import { ComparePage } from '../pages/ComparePage.tsx';
 import { FavoritesProvider } from '../../context/FavoritesContext.tsx';
 
-
-export const Root = () => (
-  <HashRouter>
-
 export const Root = () => (
   <FavoritesProvider>
     <CartProvider id="main-cart">
-    <HashRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route
-          path="/"
-          element={<App />}
-        >
+      <HashRouter>
+        <ScrollToTop />
+        <Routes>
           <Route
-            index
-            element={<HomePage />}
-          />
-          <Route
-            path="home"
-            element={
-              <Navigate
-                to="/"
-                replace
+            path="/"
+            element={<App />}
+          >
+            <Route
+              index
+              element={<HomePage />}
+            />
+            <Route
+              path="home"
+              element={
+                <Navigate
+                  to="/"
+                  replace
+                />
+              }
+            />
+
+            <Route path="phones">
+              <Route
+                index
+                element={<PhonesPage />}
               />
-            }
-          />
+              <Route
+                path=":slug"
+                element={<ItemCardPage category="phones" />}
+              />
+              <Route
+                path="*"
+                element={<PageNotFound />}
+              />
+            </Route>
 
-          <Route path="phones">
+            <Route path="tablets">
+              <Route
+                index
+                element={<TabletsPage />}
+              />
+              <Route
+                path=":slug"
+                element={<ItemCardPage category="tablets" />}
+              />
+              <Route
+                path="*"
+                element={<PageNotFound />}
+              />
+            </Route>
+
+            <Route path="accessories">
+              <Route
+                index
+                element={<AccessoriesPage />}
+              />
+              <Route
+                path=":slug"
+                element={<ItemCardPage category="accessories" />}
+              />
+              <Route
+                path="*"
+                element={<PageNotFound />}
+              />
+            </Route>
+
             <Route
-              index
-              element={<PhonesPage />}
+              path="favorites"
+              element={<FavoritesPage />}
             />
+
             <Route
-              path=":slug"
-              element={<ItemCardPage category="phones" />}
+              path="cart"
+              element={<CartPage />}
             />
+
+            <Route
+              path="contacts"
+              element={<ContactsPage />}
+            />
+
+            <Route
+              path="compare"
+              element={<ComparePage />}
+            />
+
             <Route
               path="*"
               element={<PageNotFound />}
             />
           </Route>
-
-          <Route path="tablets">
-            <Route
-              index
-              element={<TabletsPage />}
-            />
-            <Route
-              path=":slug"
-              element={<ItemCardPage category="tablets" />}
-            />
-            <Route
-              path="*"
-              element={<PageNotFound />}
-            />
-          </Route>
-
-          <Route path="accessories">
-            <Route
-              index
-              element={<AccessoriesPage />}
-            />
-            <Route
-              path=":slug"
-              element={<ItemCardPage category="accessories" />}
-            />
-            <Route
-              path="*"
-              element={<PageNotFound />}
-            />
-          </Route>
-
-          <Route
-            path="favorites"
-            element={<FavoritesPage />}
-          />
-
-          <Route
-            path="cart"
-            element={<CartPage />}
-          />
-
-          <Route
-            path="contacts"
-            element={<ContactsPage />}
-          />
-
-          <Route
-            path="compare"
-            element={<ComparePage />}
-          />
-
-          <Route
-            path="*"
-            element={<PageNotFound />}
-          />
-        </Route>
-      </Routes>
-    </HashRouter>
+        </Routes>
+      </HashRouter>
     </CartProvider>
   </FavoritesProvider>
 );
