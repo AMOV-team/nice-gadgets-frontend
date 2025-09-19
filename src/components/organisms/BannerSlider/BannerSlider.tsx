9e0339c19727d13e3d54c22f18055790414a4cbd
Max Tovstopiatyi <maxrori228@gmail.com>
2025-09-19 13:13:41 +0300

    added firt version new banner

--- src/components/organisms/BannerSlider/BannerSlider.tsx
+++ src/components/organisms/BannerSlider/BannerSlider.tsx
@@ -1,11 +1,11 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, EffectFade } from 'swiper/modules';
 import type { SwiperClass } from 'swiper/react';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 import './styles.scss';
 
 import bannerMain from '../../../../public/img/banner-slider/light-phone.png';
@@ -16,16 +16,12 @@ import { SliderButtonLeft } from '../../atoms/buttons/SliderButtonLeft';
 
 export const BannerSlider = ({ className = '' }) => {
   const swiperRef = useRef<SwiperClass | null>(null);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animationTrigger, setAnimationTrigger] = useState<number | null>(null);
 
   const slides = [
     { src: bannerMain, alt: 'Phones banner', to: '/phones' },
     { src: banner3, alt: 'Tablets banner', to: '/tablets' },
     { src: banner2, alt: 'Watches banner', to: '/accessories' },
   ];
-
-  const slideBg = ['#ffffff', '#ffffff', '#ffffff'];
 
   const handleNext = () => swiperRef.current?.slideNext();
   const handlePrev = () => swiperRef.current?.slidePrev();
@@ -36,34 +32,24 @@ export const BannerSlider = ({ className = '' }) => {
       style={{ height: '80vh' }}
     >
       <Swiper
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-        onSlideChange={(swiper) => {
-          setAnimationTrigger(swiper.realIndex);
-          setActiveIndex(swiper.realIndex);
-        }}
-        className="bannerslider"
-        spaceBetween={30}
-        centeredSlides
+        modules={[Pagination, Autoplay, EffectFade]}
+        pagination={{ clickable: true }}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
-        speed={2000}
-        pagination={{
-          clickable: true,
-          el: '.bannerslider-pagination',
-          bulletClass: 'bannerslider-bullet',
-          bulletActiveClass: 'bannerslider-bullet-active',
-        }}
         loop
-        modules={[Autoplay, Pagination, Navigation]}
+        speed={1000}
+        effect="fade"
+        fadeEffect={{ crossFade: true }}
+        className="bannerslider"
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
       >
         {slides.map((s, i) => (
           <SwiperSlide
             key={i}
-            className={`bannerslider-slide ${i === activeIndex ? 'bannerslider-slide-active' : ''}`}
+            className="bannerslider-slide"
           >
             <Link
               to={s.to}
-              className={`slide-wrapper ${i === activeIndex ? 'is-active' : ''} ${i === animationTrigger ? 'fade-in' : ''}`}
-              style={{ backgroundColor: slideBg[i] }}
+              className="slide-wrapper"
             >
               <img
                 src={s.src}
@@ -75,8 +61,7 @@ export const BannerSlider = ({ className = '' }) => {
         ))}
       </Swiper>
 
-      <div className="bannerslider-pagination" />
-
+      {/* кастомные кнопки */}
       <div className="hidden md:flex absolute left-4 top-1/2 -translate-y-1/2 z-10">
         <SliderButtonLeft
           onClick={handlePrev}
