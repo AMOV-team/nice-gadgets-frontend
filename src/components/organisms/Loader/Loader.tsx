f99adb20edcb627e96a2d4136a0421fc4f3057a5
Max Tovstopiatyi <maxrori228@gmail.com>
2025-09-25 15:25:27 +0300

    loader

--- src/components/organisms/Loader/Loader.tsx
+++ src/components/organisms/Loader/Loader.tsx
@@ -7,10 +7,13 @@ export const Loader: React.FC = () => (
                backdrop-blur-md bg-black/10 dark:bg-white/10 p-6"
   >
     <div
-      className="p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-pink-400 via-purple-400 to-indigo-600 
-        md:w-[100px] md:h-[100px] h-32 w-32 aspect-square rounded-full"
+      className="animate-spin drop-shadow-2xl 
+             md:w-[100px] md:h-[100px] h-32 w-32 
+             aspect-square rounded-full 
+             bg-gradient-to-bl from-pink-400 via-purple-400 to-indigo-600
+             [mask:radial-gradient(circle,transparent_50%,black_61%)]"
     >
-      <div className="rounded-full h-full w-full bg-slate-100 dark:bg-zinc-900 backdrop-blur-md"></div>
+      <div className="rounded-full h-full w-full bg-transparent"></div>
     </div>
   </div>
 );
